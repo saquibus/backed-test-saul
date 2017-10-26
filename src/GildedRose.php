7568cdb222543209082d1fd7ae86216e353b6b22
saquibus <saquibus@gmail.com>
2017-10-26 19:00:31 +0200

    unio del if
    uno los if para que no haya tantas condiciones

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -12,12 +12,8 @@ class GildedRose {
 
     function update_quality() {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
+            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert' and $item->quality > 0 and $item->name != 'Sulfuras, Hand of Ragnaros') {
+                 $item->quality = $item->quality - 1;
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
